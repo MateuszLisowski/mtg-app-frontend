11f0b27980c5987fa9d88d30cc442526682868bd
Mateusz Lisowski <mlmateuszlisowski@gmail.com>
2019-07-16 11:31:39 +0200

    finish Search for card logic and handle mtg card UI

--- src/views/SearchForCard.js
+++ src/views/SearchForCard.js
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { Component } from "react";
+import { toast } from "react-toastify";
+import { CardForm } from "../components/CardForm";
+import { MtgCard } from "../components/MtgCard";
+import { defaultOptions } from "../constants/toastifyOptions";
 
-export const SearchForCard = () => {
-  return (
-    <>
-      dummy code
-    </>
-  );
-};
+export class SearchForCard extends Component {
+  state = {
+    name: "",
+    types: "",
+    keywords: "",
+    text: "",
+    tournamentLegal: false,
+    attack: null,
+    defense: null,
+    foundCards: []
+  };
+
+  handleInput = (event, inputName) => {
+    const { value, type, checked } = event.target;
+
+    this.setState({
+      [inputName]: type === "checkbox" ? checked : value
+    });
+  };
+
+  searchForCards = async () => {
+    const {
+      name,
+      types,
+      keywords,
+      text,
+      tournamentLegal,
+      attack,
+      defense
+    } = this.state;
+
+    const searchCriteria = {
+      name,
+      types,
+      keywords,
+      text,
+      tournamentLegal,
+      attack,
+      defense
+    };
+
+    fetch(
+      "http://localhost:3000/card/getCards/" + JSON.stringify(searchCriteria),
+      {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(({ message, status, cards }) => {
+        if (status === 400 || status === 500) {
+          toast.error(message, defaultOptions);
+        } else {
+          toast.success(message, defaultOptions);
+          this.setState({ foundCards: cards });
+        }
+      })
+      .catch(err => {
+        toast.error(err, defaultOptions);
+      });
+  };
+
+  render() {
+    const {
+      name,
+      types,
+      keywords,
+      text,
+      tournamentLegal,
+      attack,
+      defense,
+      foundCards
+    } = this.state;
+
+    return (
+      <>
+        <h1>Search for a card based on your preferences</h1>
+        <CardForm
+          onClick={this.searchForCards}
+          buttonText={"Search"}
+          handleInput={this.handleInput}
+          name={name}
+          types={types}
+          keywords={keywords}
+          text={text}
+          tournamentLegal={tournamentLegal}
+          attack={attack}
+          defense={defense}
+          isDisabled={Boolean(
+            !name && !types && !keywords && !text && !attack && !defense
+          )}
+        />
+        <section className="flexHorizontal">
+          {Boolean(foundCards.length) && foundCards.map(card => <MtgCard />)}
+        </section>
+      </>
+    );
+  }
+}
